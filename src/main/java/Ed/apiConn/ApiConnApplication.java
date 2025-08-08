93c6679d8446d76be2a6ae66362ff7373f570138
Eduardo <eduardotigoulart@gmail.com>
2025-08-07 22:04:22 -0300

    Little Initial Api

---
+++ src/main/java/Ed/apiConn/ApiConnApplication.java
@@ -0,0 +1,13 @@
+package Ed.apiConn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ed/apiConn/ApiConnApplicationTests.java
@@ -0,0 +1,13 @@
+package Ed.apiConn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
